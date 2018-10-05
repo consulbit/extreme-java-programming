79697b9c78e70bcb6db42bc00bd7f2bc8c86b2f9
Piero Cascio <piero.cascio@gmail.com>
2018-10-05 22:40:38 +0200

    exercise_27_1

---
+++ src/com/edu/exercise_27_1/Main.java
@@ -0,0 +1,24 @@
+package com.edu.exercise_27_1;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/** Created by Pietro Cascio on 05/10/2018 */
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter directory path: ");
+    String directoryPath = scanner.next();
+    System.out.println("Enter file extension: ");
+    String fileExt = scanner.next();
+
+    Stream.of(requireNonNull(
+        new File(directoryPath).listFiles((file -> file.getName().endsWith(fileExt)))))
+        .forEach(System.out::println);
+  }
+}
