a123516c03e668dbc75ad7fd870aebef746c81a4
Piero Cascio <piero.cascio@gmail.com>
2018-10-06 09:12:15 +0200

    exercise_worksheet_2_b

--- src/com/edu/worksheet_1/exercise_2/FilteredList.java
+++ src/com/edu/worksheet_1/exercise_2/FilteredList.java
@@ -1,5 +1,7 @@
 package com.edu.worksheet_1.exercise_2;
 
+import static java.lang.String.format;
+
 import java.util.LinkedList;
 import java.util.function.Predicate;
 
@@ -8,9 +10,47 @@ import java.util.function.Predicate;
  */
 public class FilteredList<E> extends LinkedList<E> {
 
+  public static final String ELEMENT_HAS_NOT_BEEN_ACCEPTED = "Element %s has not been accepted";
   private final Predicate<E> predicate;
 
   public FilteredList(Predicate<E> predicate) {
     this.predicate = predicate;
+  }
+
+  @Override
+  public void addFirst(E e) {
+    if (predicate.test(e)) {
+      super.addFirst(e);
+    } else {
+      throw new IllegalArgumentException(format(ELEMENT_HAS_NOT_BEEN_ACCEPTED, e));
+    }
+  }
+
+  @Override
+  public void addLast(E e) {
+    if (predicate.test(e)) {
+      super.addLast(e);
+    } else {
+      throw new IllegalArgumentException(format(ELEMENT_HAS_NOT_BEEN_ACCEPTED, e));
+    }
+  }
+
+  @Override
+  public boolean add(E e) {
+    if (predicate.test(e)) {
+      return super.add(e);
+    } else {
+      throw new IllegalArgumentException(format(ELEMENT_HAS_NOT_BEEN_ACCEPTED, e));
+    }
+  }
+
+  @Override
+  public void add(int index, E element) {
+    if (predicate.test(element)) {
+      super.add(index, element);
+    } else {
+      throw new IllegalArgumentException(format(ELEMENT_HAS_NOT_BEEN_ACCEPTED, element));
+    }
+
   }
 }
