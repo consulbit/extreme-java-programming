84d487e3165808be7c6b1e1830fd9f76a3856963
Piero Cascio <piero.cascio@gmail.com>
2018-10-06 07:57:22 +0200

    exercise_worksheet_1_c

--- src/com/edu/worksheet_1/Exercise_1_a.java
+++ src/com/edu/worksheet_1/Exercise_1_a.java
@@ -4,16 +4,27 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * Write a method which fills an Integer array of a pre-defined size with
- * <strong>random positive integers</strong>. Use <strong>Arrays.setAll()</strong> to invoke
- * your random generation code. <strong>Do not iterate the array itself.</strong>
+ * Write a method which fills an Integer array of a pre-defined size with <strong>random positive
+ * integers</strong>. Use <strong>Arrays.setAll()</strong> to invoke your random generation code.
+ * <strong>Do not iterate the array itself.</strong>
  *
- * Created by Pietro Cascio on 05/10/2018
+ * <p>Created by Pietro Cascio on 05/10/2018
  */
 public class Exercise_1_a {
   public static void main(String[] args) {
     Integer[] integers = new Integer[100];
     Arrays.setAll(integers, Exercise_1_a::generateRandomNumber);
+
+    Arrays.sort(
+        integers,
+        (i1, i2) -> {
+          String value1Str = String.valueOf(i1);
+          String value2Str = String.valueOf(i2);
+          if ((value2Str.length() - value1Str.length()) == 0) {
+            return i2 - i1;
+          }
+          return value1Str.length() - value2Str.length();
+        });
 
     for (Integer integer : integers) {
       System.out.println(integer);
@@ -21,6 +32,6 @@ public class Exercise_1_a {
   }
 
   private static int generateRandomNumber(int value) {
-    return new Random().nextInt(100);
+    return new Random().nextInt(1000);
   }
 }
