99078985a53f915d985dbf1a516ede9f9e817dde
Piero Cascio <piero.cascio@gmail.com>
2018-10-13 08:32:15 +0200

    Lesson 2: Exercise at page 15. Add comment to describe the exercise.

--- src/main/java/mt/edu/um/lesson_2/Exercise_15.java
+++ src/main/java/mt/edu/um/lesson_2/Exercise_15.java
@@ -5,6 +5,18 @@ import static java.lang.System.out;
 import java.util.stream.Stream;
 
 /**
+ * <h1>Exercise</h1>
+ * <p>
+ *   <ul>
+ *     <li>
+ *       Write an infinite stream which uses a <strong>generator</strong> function to provide all
+ *       the multiples of 3, starting from 3.<br>
+ *         <i>Hint</i>: You have to use <strong>Stream.iterate()</strong>
+ *     </li>
+ *     <li>Limit it to 10 elements</li>
+ *     <li>For each element in the (finite) stream print it on screen, followed by a space</li>
+ *   </ul>
+ * </p>
  * Created by Pietro Cascio on 13/10/2018
  */
 public class Exercise_15 {
